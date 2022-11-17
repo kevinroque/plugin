8fe34efbd01af1a4f15a7406e208015cec5c6071
kevinroque1 <kevin98duarte@gmail.com>
2022-11-17 16:23:04 -0600

    primer commit

---
+++ pagos.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: pagos
+Plugin URI: https://github.com/kevinroque/plugin.git
+Description: Plugin de pagos creacion
+Version: 1.0
+Author: Kevin Eduardo Roque Duarte
+Author URI: https://github.com/kevinroque/plugin.git
+License: GPL2
+*/
